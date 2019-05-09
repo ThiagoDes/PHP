4b0ac858c89f6e122dfe660e57ede1f063d1feb5
ThiagoDes <39930138+ThiagoDes@users.noreply.github.com>
2019-05-09 09:32:39 -0300

    Update add logo
    adiciona uma logo a imagem

--- RedimensionandoCortandoImagens/cortaImage.php
+++ RedimensionandoCortandoImagens/cortaImage.php
@@ -4,9 +4,13 @@
   // height = altura
 
   $arquivo = "image.png";
+  $logo = "logo.png";
 
   $width = 200;
   $height = 200;
+
+  $width_mini = 40;
+  $height_mini = 80;
 
   list($width_original, $height_original) = getimagesize($arquivo);
 
@@ -18,10 +22,23 @@
   	$height = $width / $ratio;
   }
 
+  list($width_logo, $height_logo) = getimagesize($logo);
+
+  $ratio = $width_logo / $height_logo;
+
+  if($width_mini / $height_mini > $ratio){
+      $width_mini = $height_logo * $ratio;
+  } else {
+    $height_mini = $width_mini / $ratio;
+  }
+
   $image_final = imagecreatetruecolor($width, $height);
+  $image_logo = imagecreatefrompng($logo);
   $image_original = imagecreatefrompng($arquivo);
 
   imagecopyresampled($image_final, $image_original, 0, 0, 0, 0, $width, $height, $width_original, $height_original);
+
+  imagecopyresampled($image_final, $image_logo, 160, 70, 0, 0, $width_mini, $height_mini, $width_logo, $height_logo);
 
   header("Content-Type: image/png");
   imagepng($image_final);
